0a9acde5358009f911063bc1eb7deaadfd0312a7
zhengmianwei <zhengmianwei@163.com>
2018-03-10 21:06:45 +0800

    提交test

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -4,4 +4,7 @@ package com.test;
  * Created by Administrator on 2018/3/10.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("test------------");
+    }
 }
