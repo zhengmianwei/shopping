e9c797abbabfeb91efd8bd63e9e89506a84aa795
zhengmianwei <zhengmianwei@163.com>
2018-03-10 21:12:39 +0800

    提交test

--- src/main/java/com/test/Test.java
+++ src/main/java/com/test/Test.java
@@ -6,5 +6,6 @@ package com.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test------------");
+        System.out.println("test听得懂的------------");
     }
 }
